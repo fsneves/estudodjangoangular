fbc70d77e379641c675d78b8b9812eaecc437734
fsneves <flavioneves1@gmail.com>
2020-07-27 15:33:55 -0300

    c

---
+++ members-backend/core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Member
+from .serializers import MemberSerializer, MemberSimpleSerializer
+from rest_framework.response import Response
+
+
+# Create your views here.
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+    
+    def list(self, request, *args, **Kwargs):
+        queryset = Member.objects.all()
+        serializer = MemberSimpleSerializer(queryset, many=True)
+        return Response(serializer.data)
